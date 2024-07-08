9511e246988c55800cbf98b4f451003b689c037e
Steve <stvchrm@steves-mbp.lan>
2024-07-07 18:18:40 -0600

    set connection

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,18 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  }
+);
 
 module.exports = sequelize;
